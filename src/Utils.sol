38ff9b818da7b6c5413d9ff33aca2d08f42f257e
Andy Cernera <kooshaba@gmail.com>
2023-02-03 08:32:11 -0500

    remove utils

--- src/Utils.sol
+++ src/Utils.sol
@@ -15,24 +15,7 @@ contract Utils is DSTest {
     nextUser = keccak256(abi.encodePacked(nextUser));
     return user;
   }
-
-  //create users with 100 ether balance
-  function createUsers(uint256 userNum) external returns (address payable[] memory) {
-    address payable[] memory users = new address payable[](userNum);
-    for (uint256 i = 0; i < userNum; i++) {
-      address payable user = this.getNextUserAddress();
-      vm.deal(user, 100 ether);
-      users[i] = user;
-    }
-    return users;
-  }
-
-  //move block.number forward by a given number of blocks
-  function mineBlocks(uint256 numBlocks) external {
-    uint256 targetBlock = block.number + numBlocks;
-    vm.roll(targetBlock);
-  }
-
+  
   function getVm() public view returns (Vm) {
     return vm;
   }
