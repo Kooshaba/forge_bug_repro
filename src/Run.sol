3ae098312989a5dab2b6b9ff4a13c7c446b473dd
Andy Cernera <kooshaba@gmail.com>
2023-02-03 08:25:48 -0500

    add details

--- src/Run.sol
+++ src/Run.sol
@@ -9,16 +9,19 @@ import "forge-std/console.sol";
 
 contract Run {
     function run() public {
+        console.log("deployer", msg.sender);
+
         Utils u = new Utils();
         OwnableStorage s = new OwnableStorage();
 
         address otherUser = u.getNextUserAddress();
+        console.log("otherUser", otherUser);
 
         s.transferOwnership(otherUser);
 
         u.getVm().startPrank(otherUser);
-        LibA.publicSet(s, 1);
-        LibA.internalSet(s, 2);
+        LibA.publicSet(s, 1); // Fails
+        LibA.internalSet(s, 2); // Succeeds
         u.getVm().stopPrank();
     }
 }
